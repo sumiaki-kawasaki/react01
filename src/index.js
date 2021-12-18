d1d0b1769959bf7db6a604e65d169369ba230332
川崎澄明 <cb_clear_bright@yahoo.co.jp>
2021-12-18 16:14:58 +0000

    ゲーム勝者の判定

--- src/index.js
+++ src/index.js
@@ -27,6 +27,7 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) { return; }
     squares[i] = this.playerCharas[this.state.xIsNext];
     this.setState({
       squares: squares,
@@ -44,7 +45,13 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = `Next player: ${this.playerCharas[this.state.xIsNext]}`;
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = `Winner: ${winner}`;
+    } else {
+      status = `Next player: ${this.playerCharas[this.state.xIsNext]}`;
+    }
 
     return (
       <div>
@@ -91,3 +98,25 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
+
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
