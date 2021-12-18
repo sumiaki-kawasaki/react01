7a4b9a978265e2578f42b97157ffd97c2811fb27
川崎澄明 <cb_clear_bright@yahoo.co.jp>
2021-12-18 15:45:34 +0000

    Squareを関数コンポーネントに

--- src/index.js
+++ src/index.js
@@ -3,17 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button
-        className="square"
-        onClick={() => this.props.onClick()}
-      >
-        {this.props.value}
-      </button>
-    );
-  }
+// 関数コンポーネント(render メソッドだけを有して自分の state を持たないコンポーネント)に 
+function Square(props) {
+  return (
+    <button
+      className="square"
+      onClick={props.onClick}
+    >
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
