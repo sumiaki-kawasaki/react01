c49fbf475dca650a6fe6559e33bb2dcd3a470c75
川崎澄明 <cb_clear_bright@yahoo.co.jp>
2021-12-25 07:18:14 +0000

    タイムトラベルの実装

--- src/index.js
+++ src/index.js
@@ -55,13 +55,14 @@ class Game extends React.Component {
       history: [
         { squares: Array(9).fill(null) },
       ],
+      stepNumber: 0,
       xIsNext: 1,
     };
     this.playerCharas = [ 'O', 'X' ];
   }
 
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) { return; }
@@ -70,13 +71,21 @@ class Game extends React.Component {
       history: history.concat([{
         squares: squares,
       }]),
+      stepNumber: history.length,
       xIsNext: this.state.xIsNext === 1 ? 0 : 1,
+    });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step + 1) % 2,
     });
   }
 
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
 
     const moves = history.map((step, move) => {
