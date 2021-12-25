6c0fd6cf71bb022854742c88abebf9dc628012fd
川崎澄明 <cb_clear_bright@yahoo.co.jp>
2021-12-25 07:10:58 +0000

    過去の着手の表示

--- src/index.js
+++ src/index.js
@@ -78,6 +78,16 @@ class Game extends React.Component {
     const history = this.state.history;
     const current = history[history.length - 1];
     const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const desc = move ? `Go to move #${move}` : 'Go to game start';
+      return (
+        <li>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+
     const status = (winner) ?
       `Winner: ${winner}` :
       `Next player: ${this.playerCharas[this.state.xIsNext]}`;
@@ -92,7 +102,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
